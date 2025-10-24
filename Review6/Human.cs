d69eca5f1f99f07d1ec9a000eaa8bd48ed39c34f
RaniaEssam <ranon6389@gmail.com>
2025-10-24 20:49:37 +0300

    Add files via upload

---
+++ Review6/Human.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class Human
+    {
+        public string name;
+        public int id;
+        bankaccont bk;
+
+        public override bool Equals(object obj)
+        {
+            // Human h = (Human)obj; // not a safe casting 
+            // if the obj is not of type HUMAN the casting isnot applied
+            //// to solve this problem
+            //Human h = obj as Human;
+            //// if the obj is not from class human
+            //// h == NULL
+            //if (h == null) return false;
+            //return this.id == h.id;
+
+            // to check if obj from class humna 
+            if (obj is Human)
+            {
+                Human h = obj as Human;
+                return this.id == h.id;
+            }
+            else return false;
+
+        }
+        public override string ToString()
+        {
+            return $"{name} {id}";
+        }
+
+        public Human Clone()
+        {
+            Human h = this.MemberwiseClone() as Human;
+            h.bk = new bankaccont(); // deep copy 
+            h.bk.id = this.bk.id;
+            h.bk.name = this.bk.name;
+            return h;
+
+        }
+    }
+}
---
+++ Review6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Day6
+{
+    internal class Program
+    {
+   
+        static void Main(string[] args)
+        {
+            Human h = new Human();
+            h.name = "rania";
+            h.id = 1000;
+            Human hh = new Human();
+            hh.name = "mo_to_ACPC";
+            hh.id = 100;
+
+            //Console.WriteLine(object.Equals(h, hh));
+            Console.WriteLine(h.ToString());
+            Console.WriteLine(h);
+
+            Human h2 = h.Clone();
+
+            Console.WriteLine($"{h2.id} {h2.name}");
+           
+           
+        }
+    }
+}
